348223e6e22cf00177b1b4bd864dd7678eade1a9
Emmanuel Castaño Sepúlveda <ecastanos@eafit.edu.co>
2026-04-10 11:41:32 -0500

    solución de errores

--- frontend/.next/dev/static/chunks/app_poc_page_tsx_893e5ca7._.js
+++ frontend/.next/dev/static/chunks/app_poc_page_tsx_893e5ca7._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_9a008d56._.js"
+  "static/chunks/_0e54cfeb._.js"
 ],
     source: "dynamic"
 });
